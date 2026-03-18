cabe22a029db7dc7a1b8f2390311239dca7b70f5
lambdatiger <spam.stnuocca.sl@gmail.com>
2026-03-18 00:48:01 -0500

    static GRAD version, could be a submodule in the future

---
+++ modules/grad-vehicleSpawner/functions/script_macros.hpp
@@ -0,0 +1,8 @@
+#define PREFIX grad
+#define COMPONENT vehicleSpawner
+#include "\x\cba\addons\main\script_macros_mission.hpp"
+
+#define MAX_VEHICLE_JETS 3
+#define MAX_VEHICLE_HELI 4
+#define MAX_VEHICLE_ARMR 2
+#define MAX_VEHICLE_ARMD 4
